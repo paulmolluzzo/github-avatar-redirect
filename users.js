790c5e5206842e6b12fec63e0997f1e155add85a
Paul Molluzzo <paul@molluzzo.com>
2016-04-01 16:01:42 -0400

    ES2015 support; Procfile to build on heroku

--- users.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var Users = new Schema({
-  username: String,
-  avatar: String
-});
-
-module.exports = mongoose.model('User', Users);
